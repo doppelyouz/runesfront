fd56b7a683f4760ffc31433fccb684da4d0c2025
counterat <counteratios@gmail.com>
2024-05-31 11:25:22 +0300

    sdsd

--- src/constants/shop.js
+++ src/constants/shop.js
@@ -1,37 +1,45 @@
+import runeMiddle from 'src/assets/GeneralPage/rune middle.png';
+import rune1 from 'src/assets/GeneralPage/rune 1.png';
+import rune2 from 'src/assets/GeneralPage/rune 2.png';
+import rune3 from 'src/assets/GeneralPage/rune 3.png';
+import rune4 from 'src/assets/GeneralPage/rune 4.png';
+
+
+
 export default [
   {
     id: 0,
     count: 0.1,
     profitFehu: 10,
     profitTon: 0,
-    icon: "/src/assets/GeneralPage/rune middle.png",
+    icon: runeMiddle,
   },
   {
     id: 1,
     count: 0.3,
     profitFehu: 5,
     profitTon: 0.1,
-    icon: "/src/assets/GeneralPage/rune 1.png",
+    icon: rune1,
   },
   {
     id: 2,
     count: 0.5,
     profitFehu: 10,
     profitTon: 0.2,
-    icon: "/src/assets/GeneralPage/rune 3.png",
+    icon: rune3,
   },
   {
     id: 3,
     count: 1,
     profitFehu: 25,
     profitTon: 0.4,
-    icon: "/src/assets/GeneralPage/rune 2.png",
+    icon: rune2,
   },
   {
     id: 4,
     count: 3,
     profitFehu: 80,
     profitTon: 1.3,
-    icon: "/src/assets/GeneralPage/rune 4.png",
+    icon: rune4,
   },
 ];
